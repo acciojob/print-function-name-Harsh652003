e3f344a6e8e5c8bac9ae93706d905dd134e7c7e7
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-10 01:58:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//your JS code here. If required.
- function myFunction () {
-  alert(myFunction.name); // Outputs: "myFunction"
+//your JS code here. If requi
+const  myFunction= function () {
+  alert(arguments.callee.name); 
 };
 
 myFunction();
